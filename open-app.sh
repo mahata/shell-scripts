ca70ce0ce364b745deda258398aba25e08d4fc16
mahata <mahata777@gmail.com>
2017-08-02 15:13:33 +0900

    Add initial version of open-app.sh

---
+++ open-app.sh
@@ -0,0 +1,57 @@
+#!/bin/bash -e
+
+PROGNAME=$(basename $0)
+VERSION="0.1"
+
+usage() {
+    echo "Usage: $PROGNAME [OPTIONS] APP"
+    echo "  This script is ~."
+    echo
+    echo "Options:"
+    echo "  -h, --help"
+    echo "      --version"
+    echo
+    exit 1
+}
+
+for OPT in "$@"
+do
+    case "$OPT" in
+        '-h'|'--help' )
+            usage
+            exit 1
+            ;;
+        '--version' )
+            echo $VERSION
+            exit 1
+            ;;
+        '--'|'-' )
+            shift 1
+            param+=( "$@" )
+            break
+            ;;
+        -*)
+            echo "$PROGNAME: illegal option -- '$(echo $1 | sed 's/^-*//')'" 1>&2
+            exit 1
+            ;;
+        *)
+            if [[ ! -z "$1" ]] && [[ ! "$1" =~ ^-+ ]]; then
+                param+=( "$1" )
+                shift 1
+            fi
+            ;;
+    esac
+done
+
+if [ -z $param ]; then
+    echo "$PROGNAME: too few arguments" 1>&2
+    echo "Try '$PROGNAME --help' for more information." 1>&2
+    exit 1
+fi
+
+if [[ $(ps ax | grep $param | grep -v grep | grep -v $PROGNAME | wc -l) -eq 0 ]]; then
+    open $param
+    exit 0
+fi
+
+exit 1
